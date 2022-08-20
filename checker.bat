9a911e09d191841f3c874972d7cb636563da188a
LeonaRaging <97384817+LeonaRaging@users.noreply.github.com>
2022-08-20 18:21:49 +0700

    Update checker.bat

--- checker.bat
+++ checker.bat
@@ -6,12 +6,8 @@ for /l %%x in (1, 1, 100) do (
     main__Generator > inputf.in
     main < inputf.in > test.out 
     main__Good < inputf.in > outputf.out
-    fc /b test.out outputf.out > nul
-    if errorlevel 1 (
-	exit /b
-    ) else (
-	echo Test #%%x :CORRECT!
-    )
+    fc outputf.out test.out > diagnostics || exit /b
+    echo Test #%%x :CORRECT!
 )
 echo all tests passed
 pause
