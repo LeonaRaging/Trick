c5c86c0a61c213cd9917ce52edcbbc592d8b115c
LeonaRaging <97384817+LeonaRaging@users.noreply.github.com>
2023-02-21 11:23:07 +0700

    Update Matrix_Expo.cpp

--- Matrix_Expo.cpp
+++ Matrix_Expo.cpp
@@ -1,28 +1,27 @@
 struct Matrix {
-    int n, m;
-    vector<vector<ll>> a;
-    void init() {
-        a.resize(n + 1);
-        for (int i = 1; i <= n; i++) a[i].resize(m + 1);
-    }
+	int r, c;
+	vector<vector<int>> a;
+	Matrix (int r = 0, int c = 0): r(r), c(c) {
+		a.resize(r + 4);
+		for (int i = 0; i <= r; i++)
+			a[i].resize(c + 4);
+	}
 };
 
-Matrix Mul(Matrix A, Matrix B)
-{
-    int n = A.n, m = B.m, k = A.m;
-    Matrix C; C.n = n; C.m = m; C.init();
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++)
-            for (int t = 1; t <= k; t++)
-                C.a[i][j] = (C.a[i][j] + (A.a[i][t] * B.a[t][j]) % mod) % mod;
-    return C;
+Matrix Mul(Matrix A, Matrix B) {
+	int r = A.r, c = B.c, k = A.c;
+	Matrix C(r, c);
+	for (int i = 1; i <= r; i++)
+		for (int j = 1; j <= c; j++)
+			for (int t = 1; t <= k; t++)
+				C.a[i][j] = (C.a[i][j] + A.a[i][t] * B.a[t][j]);
+	return C;
 }
 
-Matrix Pow(Matrix A, ll b)
-{
-    if (b == 1) return A;
-    Matrix C = Pow(A, b / 2);
-    C = Mul(C, C);
-    if (b % 2 != 0) C = Mul(C, A);
-    return C;
+Matrix Pow(Matrix A, ll b) {
+	if (b == 1) return A;
+	Matrix C = Pow(A, b / 2);
+	C  = Mul(C, C);
+	if (b & 1) C = Mul(C, A);
+	return C;
 }
