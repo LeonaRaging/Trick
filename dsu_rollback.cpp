b14b1af23e8bf7cdfe02e1eb626bca8097a6d81c
LeonaRaging <97384817+LeonaRaging@users.noreply.github.com>
2024-10-24 21:46:12 +0700

    Update dsu_rollback.cpp

--- dsu_rollback.cpp
+++ dsu_rollback.cpp
@@ -1,21 +1,30 @@
-struct RollbackUF {
-	vi e; vector<pii> st;
-	RollbackUF(int n) : e(n, -1) {}
-	int size(int x) { return -e[find(x)]; }
-	int find(int x) { return e[x] < 0 ? x : find(e[x]); }
-	int time() { return sz(st); }
-	void rollback(int t) {
-		for (int i = time(); i --> t;)
-			e[st[i].first] = st[i].second;
-		st.resize(t);
-	}
-	bool join(int a, int b) {
-		a = find(a), b = find(b);
-		if (a == b) return false;
-		if (e[a] > e[b]) swap(a, b);
-		st.push_back({a, e[a]});
-		st.push_back({b, e[b]});
-		e[a] += e[b]; e[b] = a;
-		return true;
-	}
-};
+struct DisjointSet {
+    int par[maxn], Rank[maxn];
+    stack<int> st;
+    void init(int n) {
+        for (int i = 1; i <= n; i++)
+            par[i] = i, Rank[i] = 1;
+    }
+    int find(int x) {
+        while (x != par[x]) x = par[x];
+        return x;
+    }
+    bool join(int u, int v) {
+        u = find(u), v = find(v);
+        if (u == v) return 0;
+        if (Rank[u] < Rank[v]) swap(u, v);
+        num--;
+        Rank[u] += Rank[v];
+        par[v] = u;
+        st.push(v);
+        return 1;
+    }
+    void rollback(int sz) {
+        while (SZ(st) > sz) {
+            int v = st.top(); st.pop();
+            Rank[par[v]] -= Rank[v];
+            num++;
+            par[v] = v;
+        }
+    }
+} Dsu;
