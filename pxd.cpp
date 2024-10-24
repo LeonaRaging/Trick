127b68be037ed73bf38aa5a6c2573c319b8d701f
Ho Sy Phat <65226220+R4ptorVN@users.noreply.github.com>
2024-10-24 21:16:56 +0700

    Create pxd.cpp

---
+++ pxd.cpp
@@ -0,0 +1,56 @@
+|///HO SY PHAT - K69 UET - TERROBYTE
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+#define Unique(x) (x).resize(distance((x).begin(), unique(all((x)))))
+#define getbit(x, i) ((x >> i) & 1)
+#define biton(x, i) (x | (1 << i))
+#define bitoff(x, i) (x & ~(1 << i))
+#define cntbit(x) (__builtin_popcount(x))
+
+const ll mod = 1e9 + 7;
+const ll base = 31;
+const ll nmax = 1000005;
+
+int n;
+void solve()
+{
+     cin>>n;
+}
+int main()
+{
+    //freopen(".inp", "r", stdin);
+    //freopen(".out", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+    //cin>>T;
+    while(T--)
+    {
+         solve();
+         //cout<<'\n';
+    }
+}
+///REMEMBER TO BE CUTE
+/*
+---SAMPLE GOES HERE---
+*/
+
+
+
+
+
+
+
+
+
+
+
+
