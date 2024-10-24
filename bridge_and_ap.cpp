d00a1399e5cb4a760ac86d3b79ef9b7cc4869015
LeonaRaging <97384817+LeonaRaging@users.noreply.github.com>
2024-10-24 21:44:51 +0700

    Update bridge_and_ap.cpp

--- bridge_and_ap.cpp
+++ bridge_and_ap.cpp
@@ -1,31 +1,30 @@
-int low[nmax];
-int num[nmax];
-int child[nmax];
-bool vis[nmax];
-bool arti[nmax];
-bool bridge[nmax];
-int cnt = 0;
-void dfs_bridge_ap(int u)
-{
-     low[u] = n + 1;
-     num[u] = ++cnt;
-     for(auto v : a[u])
-     {
-         if(!vis[v.se])
-         {
-             vis[v.se] = 1;
-             if(!num[v.fi])
-             {
-                child[u]++;
-                dfs_bridge_ap(v.fi);
-                low[u] = min(low[u], low[v.fi]);
-                if(low[v.fi] > num[u])
-                    bridge[v.se] = 1;
-                if(low[v.fi] >= num[u])
-                    arti[u] = 1;
-             }
-             else
-                 low[u] = min(low[u], num[v.fi]);
-         }
-     }
+struct Edge {
+	int u, v;
+	bool used, bridge;
+	int get(int x) {return u ^ v ^ x;}
+	Edge(int u = 0, int v = 0): u(u), v(v) {
+		used = bridge = false;
+	}
+};
+
+int n, m, root, tin[maxn], low[maxn], Point[maxn];
+vector<Edge> edges;
+vector<int> adj[maxn];
+
+void dfs(int u) {
+	int child = 0;
+	tin[u] = low[u] = ++tin[0];
+	for (int i : adj[u]) if (!edges[i].used) {
+		edges[i].used = 1;
+		int v = edges[i].get(u);
+		if (!tin[v]) {
+			dfs(v);
+			child++;
+			low[u] = min(low[u], low[v]);
+			edges[i].bridge = low[v] > tin[u];
+			if (u == root) Point[u] = child > 1;
+			else Point[u] |= low[v] >= tin[u];
+		}
+		else low[u] = min(low[u], tin[v]);
+	}
 }
